fade0c9e3d5a717680b5453294bb40add425db8b
missthesnow <towhitney@gmail.com>
2020-10-18 00:05:30 -0600

    Removed extraneous files

--- Weeks 04-05 - Cloud Databases/web/functions/index.js
+++
@@ -1,7 +0,0 @@
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
-
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
-
-// TODO(DEVELOPER): Write the blurOffensiveImages Function here.
-
-// TODO(DEVELOPER): Write the sendNotifications Function here.
